a4941d54b5061303b90b8b991f6cbdde417d32fc
KUMAR UTSAV <utsavk686@gmail.com>
2024-10-01 23:06:39 +0530

    Time: 0 ms (100%), Space: 42.2 MB (29.4%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,8 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //two pointer approach
         int i = m-1;
         int j = n-1;
-        int k = m+n-1; 
+        int k=m+n-1;
 
         while(j>=0){
             if(i>=0 && nums1[i]>nums2[j]){
@@ -12,7 +11,7 @@ class Solution {
                 i--;
             }
             else{
-                nums1[k]= nums2[j];
+                nums1[k] = nums2[j];
                 k--;
                 j--;
             }
