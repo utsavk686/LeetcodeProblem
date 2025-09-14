52909316777e2d9b0a2ff74b9c6d6f7a9649cea4
KUMAR UTSAV <utsavk686@gmail.com>
2025-09-14 12:23:19 +0530

    Time: 2 ms (98.83%), Space: 42.8 MB (95.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        // Two Pointer Approach
-
-        int left = 0;
+        int left=0;
         int right = s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
 
-        while(left<right){// left!=right because we are not comparing the middle element in palindrome e.g, Ata 
-
-        while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
-            left++;
-        }
-        while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
-            right--;
-        }
-
-        // comparing the digit are equal from both left and right side
-        if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
-            return false;
-        }
+            if(Character.toLowerCase(s.charAt(left))!=Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
             left++;
             right--;
         }
