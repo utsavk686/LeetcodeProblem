22191094953d23f29cb643f8e3cc5727d9b79c39
KUMAR UTSAV <utsavk686@gmail.com>
2026-03-30 07:30:39 +0530

    Time: 6 ms (13.91%), Space: 49.4 MB (5.33%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,4 +1,5 @@
 class Solution {
+    // note - this solution is not valid for this questions as it is asking to complete in O(1) space, but here we are using a hashmap so space complexity became O(n)
     public int[] twoSum(int[] numbers, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0;i<numbers.length;i++){
