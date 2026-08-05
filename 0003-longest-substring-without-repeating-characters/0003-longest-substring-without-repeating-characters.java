98a453c15a1f2b52589505c95246b644e231f8d3
KUMAR UTSAV <utsavk686@gmail.com>
2026-08-06 01:05:34 +0530

    Time: 79 ms (7.79%), Space: 47.5 MB (9.86%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,31 +1,31 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int start=0;
+        int left = 0;
         int maxLength = 0;
         HashMap<Character, Integer> map = new HashMap<>();
-        //expand the window
-        for(int end =0; end<s.length();end++){
-            char rightChar = s.charAt(end);
-            if(map.containsKey(rightChar)){
-                map.put(rightChar,map.get(rightChar)+1);
+        //step1: expand the window
+        for(int end = 0; end<s.length(); end++){
+            char endChar = s.charAt(end);
+            //step2: Update window data
+            if(map.containsKey(endChar)){
+                map.put(endChar, map.get(endChar)+1);
             }else{
-                map.put(rightChar, 1);
+                map.put(endChar, 1);
             }
-            //if the window is valid, update the answer
-            if(map.size()==end-start+1){
-                maxLength = Math.max(maxLength, end-start+1);
-            }
-            //if window is invalid, correct the window(Shrink from left)
-            while(map.size()<end-start+1){
-                char leftChar = s.charAt(start);
+            //step 3: correct invalid window
+            while(map.size()<end-left+1){
+                //shrink from left
+                char leftChar = s.charAt(left);
                 map.put(leftChar, map.get(leftChar)-1);
 
-                //if after removing from left, the value bcome 0, remove that character from HashMap
                 if(map.get(leftChar)==0){
                     map.remove(leftChar);
                 }
-                start++;
+                left++;
             }
+
+            // now the window is valid, update the answer
+            maxLength = Math.max(maxLength, end-left+1);
         }
         return maxLength;
     }
